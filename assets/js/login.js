399df574be1e866c5c8662932870024e48e5dbd4
jocelyn <499264478@qq.com>
2020-10-30 19:03:15 +0800

    03

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,7 +2,7 @@ $(function () {
   $('#link_login').on('click', function () {
     $('.loginBox').hide()
     $('.regBox').show()
-    console.log('nt');
+    // console.log('nt');
 
   })
   $('#link_reg').on('click', function () {
@@ -13,30 +13,32 @@ $(function () {
   //自定义校验规则
   let form = layui.form;
   form.verify({
-    pwd:  [
-    /^[\S]{6,12}$/
-    ,'密码必须6到12位，且不能出现空格'
-  ] ,
+    pwd: [
+      /^[\S]{6,12}$/
+      , '密码必须6到12位，且不能出现空格'
+    ],
     repwd: function (value) {
       let pwd = $('.regBox [name=password]').val()
       if (value !== pwd) {
-          return '两次密码输入不一致'
+        return '两次密码输入不一致'
       }
     }
   })
-  //给注册表单绑定点击事件
-  $('.regBox').on('submit',function(e){
+  //给监听表单提交事件
+  $('#reg_form').on('submit', function (e) {
     e.preventDefault()
     var data = {
-      username:$('.regBox [name=username]').val(),
-      pwd:$('.regBox [name=password').val()
-}
-    $.post('http://ajax.frontend.itheima.net/api/reguser',data,function(res){
+      username: $('#reg_form [name=username]').val(),
+      password: $('#reg_form [name=password]').val()
+    }
+    $.post('http://ajax.frontend.itheima.net/api/reguser', data, function (res) {
       console.log(res)
+      if(res.status !== 0) return console.log(res.message)
+      console.log('注册成功')
 
-})
+    })
 
-})
+  })
 
 
 })
