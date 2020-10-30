881d72b031160831eed6224e39a842b8f115f7eb
jocelyn <499264478@qq.com>
2020-10-30 16:04:40 +0800

    01

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,4 +9,33 @@ $(function () {
     $('.regBox').hide()
 
   })
+  //自定义校验规则
+  let form = layui.form;
+  form.verify({
+    pwd:  [
+    /^[\S]{6,12}$/
+    ,'密码必须6到12位，且不能出现空格'
+  ] ,
+    repwd: function (value) {
+      let pwd = $('.regBox [name=password]').val()
+      if (value !== pwd) {
+          return '两次密码输入不一致'
+      }
+    }
+  })
+  //给注册表单绑定点击事件
+  $('.regBox').on('submit',function(e){
+    e.preventDefault()
+    var data = {
+      username:$('.regBox [name=username]').val(),
+      pwd:$('.regBox [name=password').val()
+}
+    $.post('http://ajax.frontend.itheima.net/api/reguser',data,function(res){
+      console.log(res)
+
+})
+
+})
+
+
 })
