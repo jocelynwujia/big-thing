f66d3804a1576ddde0c99a5db72c5ac9af3ea348
jocelyn <499264478@qq.com>
2020-10-31 11:01:18 +0800

    '登录页面请求'

--- assets/js/login.js
+++ assets/js/login.js
@@ -25,7 +25,7 @@ $(function () {
       }
     }
   })
-  //给监听表单提交事件
+  //监听注册表单提交事件
   $('#reg_form').on('submit', function (e) {
     e.preventDefault()
     var data = {
@@ -34,12 +34,35 @@ $(function () {
     }
     $.post('http://ajax.frontend.itheima.net/api/reguser', data, function (res) {
       console.log(res)
-      if(res.status !== 0) return layer.msg(res.message)
+      if (res.status !== 0) return layer.msg(res.message)
       layer.msg('注册成功')
-
+      //自动触发登录页面的点击事件
+      $('#link_reg').click()
     })
 
   })
 
+  //监听登录表单提交事件
+  $('#form_login').submit(function(e){
+    //阻止表单提交行为
+    e.preventDefault()
+    //快速获取表单数据
+    var data = $('#form_login').serialize()
 
+    console.log(data)
+    $.ajax({
+      type:'POST',
+      url:'http://ajax.frontend.itheima.net/api/login',
+      data,   
+      success:function(res){
+          console.log(res)
+          if(res.status !== 0) layer.msg(res.message)
+          layer.msg('登录成功')
+          //拿到的数据里面有一个token ，是访问有权限接口的 存储到本地存储
+          localStorage.setItem('token',res.token)
+          //同时页面跳转到后台页面首页
+          location.href = '/index.html'
+}
+})
+})
 })
