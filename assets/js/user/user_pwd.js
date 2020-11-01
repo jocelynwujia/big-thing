88bb848f75b8019e1cf7d1e4f1fac9851a94ba56
jocelyn <499264478@qq.com>
2020-11-01 19:43:13 +0800

    重置密码

--- assets/js/user/user_pwd.js
+++ assets/js/user/user_pwd.js
@@ -8,16 +8,38 @@ $(function () {
     ],
     samePwd: function (value) {
       if (value === $('[name=oldPwd]').val()) {
-          return '新旧密码不能一致'
+        return '新旧密码不能一致'
       }
     },
     repwd: function (value) {
       if (value !== $('[name=newPwd]').val()) {
-          return '两次密码不一致'
+        return '两次密码不一致'
 
       }
 
     },
+
+  })
+
+  // 重置密码
+  //给表单绑定submit提交
+  $('.layui-form').on('submit', function (e) {
+    //阻止表单的默认行为
+    e.preventDefault()
+    $.ajax({
+      method: 'POST',
+      url: '/my/updatepwd',
+      data: $(this).serialize(),
+      success: function (res) {
+        console.log(res)
+        if(res.status !== 0) return layui.layer.msg('更新密码失败')
+        layui.layer.msg('更新密码成功')
+        //重置表单 （清空表单）
+        $('.layui-form')[0].reset()
+
+      }
+
+    })
 
   })
 })
