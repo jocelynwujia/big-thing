6d5f4ffe2943c98253f570c982b03caa2fbdaff6
jocelyn <499264478@qq.com>
2020-11-01 21:58:37 +0800

    log

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -12,4 +12,28 @@ $(function () {
   // 1.3 创建裁剪区域
   $image.cropper(options)
 
+  //为上传按钮绑定点击事件
+  $('#btnSubmitImage').on('click', function () {
+    $('#file').click()
+
+  })
+  //给文件筐绑定change事件
+  $('#file').on('change', function (e) {
+    console.log(e.target.files)
+    var fileList = e.target.files
+    console.log(fileList)
+    if (fileList.length === 0) {
+      return layui.layer.msg('请选择图片')
+    }
+    //拿到用户选择的文件
+    var file = e.target.files[0]
+    // 根据选择的文件，创建一个对应的 URL 地址：
+    var newImgURL = URL.createObjectURL(file)
+    // 先`销毁`旧的裁剪区域，再`重新设置图片路径`，之后再`创建新的裁剪区域`：
+    $image
+      .cropper('destroy')      // 销毁旧的裁剪区域
+      .attr('src', newImgURL)  // 重新设置图片路径
+      .cropper(options)        // 重新初始化裁剪区域
+  })
+
 })
